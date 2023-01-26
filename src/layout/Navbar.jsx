b84bfb9f3198acc0b8f4eebcf73654175d145c93
Andrés Fernández <andresavena19@gmail.com>
2023-01-26 14:54:34 -0500

    feat: added navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
 	return (
 		<>
-			<div>Navbar</div>
-			<Outlet />
-			<div>Footer</div>
+			<nav className="navbar navbar-expand-sm bg-light">
+				<div className="container-fluid">
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+				</div>
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<Link className="nav-link" to="/" >
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" to="/users" >
+								Users
+							</Link>
+						</li>
+
+					</ul>
+				</div>
+			</nav>
+			<div className="container p-2">
+				<Outlet />
+
+			</div>
 		</>
 	)
 }
