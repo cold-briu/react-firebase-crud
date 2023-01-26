f8782616aff07ea2be61c7a6a8151b6c2462a4e0
Andrés Fernández <andresavena19@gmail.com>
2023-01-26 15:11:13 -0500

    feat: added state to user context

--- src/context/users.js
+++ src/context/users.js
@@ -1,12 +1,34 @@
 import React, { createContext, useState } from 'react'
+import { users as usersApi } from "../api";
 
 export const usersContext = createContext()
 
 export const UsersProvider = ({ children }) => {
 
+	const [data, setData] = useState(null)
+	const [error, setError] = useState("")
+	const [isLoading, setIsLoading] = useState(false)
+
+	const getData = async () => {
+		try {
+			setData(null)
+			setIsLoading(true)
+			const res = await usersApi.getUsers()
+			setIsLoading(false)
+			setData(res.data)
+		} catch (error) {
+			setIsLoading(false)
+			setError(error.message)
+		}
+	}
+
+	const state = { data, error, isLoading };
+
 	return (
 		<>
-			{children}
+			<usersContext.Provider value={state}>
+				{children}
+			</usersContext.Provider>
 		</>
 	)
 }
