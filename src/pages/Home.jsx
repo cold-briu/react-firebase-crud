17270859ec61c2e8da92c053a732373f690b1678
Andrés Fernández <andresavena19@gmail.com>
2023-02-03 17:47:48 -0500

    feat: added firebase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
 	return (
-		<div>Home</div>
+		<>
+			<div>Home</div>
+			<img src="https://pbs.twimg.com/media/Cezrs0bWAAElw9i.jpg" alt="bendecido dia" />
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/04854XqcfCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+		</>
 	)
 }
 
